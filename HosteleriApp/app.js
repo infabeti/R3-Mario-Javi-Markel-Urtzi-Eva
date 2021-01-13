5085e528b4aadeac2e42b728ebeba93791167b49
in2daw <in2daw@elorrieta.int>
2021-01-13 16:35:57 +0100

    Esqueleto básico Express app

---
+++ HosteleriApp/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '\\public\\index.html'));
+  
+  
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
