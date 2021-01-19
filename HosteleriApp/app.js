ab302ea07de15a3a64f922c0cc9a65c4faf61c05
mariosoler <mario.solerga@elorrieta-errekamari.com>
2021-01-19 19:38:06 +0100

    Mensaje de error con status 404

--- HosteleriApp/app.js
+++ HosteleriApp/app.js
@@ -4,12 +4,22 @@ const path = require('path');
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '\\public\\index.html'));
+  
+  res.sendFile(path.join(__dirname + '\\public\\index.html'),function(error){
+    if(error){
+      res.status(404).send("<h2>error 404: No existe o no se encuentra el archivo \"index.html\"</h2>");
+    }
+
+  });
+  
+  
+
+
   
   
 });
 app.use(express.static(path.join(__dirname, "public"),{extensions: ['html']} ));
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('La app está escuchando desde el puerto 8000!')
 });
