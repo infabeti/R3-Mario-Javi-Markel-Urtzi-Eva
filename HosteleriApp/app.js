04cdb5ba06064c773e56d38510c4f18b0a944b92
MarkelCuesta <markel.cuestaar@elorrieta.errekamari.com>
2021-01-22 12:09:08 +0100

    Añadiendo error404 en las demás páginas y plantilla de Bares hecha, pendiente cambiar contenido

--- HosteleriApp/app.js
+++ HosteleriApp/app.js
@@ -2,23 +2,52 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, "public"),{extensions: ['html']} ));
 
+//Error pagina principal
 app.get('/', (req, res) => {
-  
   res.sendFile(path.join(__dirname + '\\public\\index.html'),function(error){
     if(error){
-      res.status(404).send("<h2>error 404: No existe o no se encuentra el archivo \"index.html\"</h2>");
+      res.status(404).send("<h2>error 404: No existe o no se encuentra la página principal</h2>");
     }
 
   });
   
-  
+});
 
+// Error index
+app.get('/restaurantes', (req, res) => {
+	  res.sendFile(path.join(__dirname + '\\public\\restaurantes.html'),function(error){
+	    if(error){
+	      res.status(404).send('<h2>error 404: No existe o no se encuentra el archivo "restaurantes.html"</h2>');
+	    }
 
-  
+	  });
   
 });
-app.use(express.static(path.join(__dirname, "public"),{extensions: ['html']} ));
+
+//Error cafeterias
+app.get('/cafeterias', (req, res) => {
+  res.sendFile(path.join(__dirname + '\\public\\cafeterias.html'),function(error){
+    if(error){
+      res.status(404).send('<h2>error 404: No existe o no se encuentra el archivo "cafeterias.html"</h2>');
+    }
+
+  });
+  
+});
+
+// Error bares
+app.get('/bares', (req, res) => {
+  res.sendFile(path.join(__dirname + '\\public\\bares.html'),function(error){
+    if(error){
+      res.status(404).send('<h2>error 404: No existe o no se encuentra el archivo "bares.html"</h2>');
+    }
+
+  });
+  
+});
+
 
 app.listen(8000, () => {
   console.log('La app está escuchando desde el puerto 8000!')
