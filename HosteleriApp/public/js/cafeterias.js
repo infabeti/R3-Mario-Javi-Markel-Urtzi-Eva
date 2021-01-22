5ab9f0c77b050daf45278cf3b0241b0c64e6a7dd
MarkelCuesta <markel.cuestaar@elorrieta.errekamari.com>
2021-01-22 17:14:13 +0100

    diseñando dropdown bares(sin terminar)

--- HosteleriApp/public/js/cafeterias.js
+++ HosteleriApp/public/js/cafeterias.js
@@ -1,10 +1,18 @@
 function MapsListo() {
 	initMap(document.getElementById("map1"),43.28204226867892, -2.9633951729221835);
+
 	initMap(document.getElementById("map2"),43.27673088861441, -2.957610908458241);
+
 	initMap(document.getElementById("map3"),43.28153704029055, -2.9630619653246835);
+
 	initMap(document.getElementById("map4"),43.27482776711705, -2.9741796688535924);
+
 	distanciaCliente($("#caf1"),43.28204226867892, -2.9633951729221835)
+
 	distanciaCliente($("#caf2"),43.27673088861441, -2.957610908458241)
+
 	distanciaCliente($("#caf3"),43.28153704029055, -2.9630619653246835)
+
 	distanciaCliente($("#caf4"),43.27482776711705, -2.9741796688535924)
+
 }
